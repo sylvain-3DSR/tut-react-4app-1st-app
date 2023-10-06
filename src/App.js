b5393ab00ce026573e04e387da536f99ff810f62
sylvain-3DSR <58294074+sylvain-3DSR@users.noreply.github.com>
2023-10-06 14:32:10 +0200

    Main CSS SETUP

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css'
 const App = () => {
   return (
     <div className="App">
-        <div className ="gradient_bg">
+        <div className ="gradient__bg">
           <Navbar />
           <Header />
         </div>
